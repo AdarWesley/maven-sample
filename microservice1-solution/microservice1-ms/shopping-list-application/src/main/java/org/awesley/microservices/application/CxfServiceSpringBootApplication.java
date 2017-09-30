eab47ff04e534ee1e01a9e131392a8782f750432
Adar Wesley <Adar.Wesley@gmail.com>
2017-09-30 16:35:46 +0300

    Renaming folders and files to make them generic

--- microservice1-solution/microservice1-ms/shopping-list-application/src/main/java/org/awesley/microservices/application/CxfServiceSpringBootApplication.java
+++ microservice1-solution/microservice1-ms/shopping-list-application/src/main/java/org/awesley/microservices/application/CxfServiceSpringBootApplication.java
@@ -1,4 +1,4 @@
-package org.awesley.samples;
+package org.awesley.microservice1.application;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,10 +11,9 @@ import org.apache.cxf.Bus;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.swagger.Swagger2Feature;
-import org.awesley.shoppinglist.persistence.configuration.PersistenceConfiguration;
-import org.awesley.shoppinglist.resources.configuration.ResourcesConfiguration;
-import org.awesley.shoppinglist.service.configuration.ServicesConfiguration;
-//import org.awesley.shoppinglist.resources.implementation.ShoppingListApiImpl;
+import org.awesley.microservice1.persistence.configuration.PersistenceConfiguration;
+import org.awesley.microservice1.resources.configuration.ResourcesConfiguration;
+import org.awesley.microservice1.service.configuration.ServicesConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -53,11 +52,8 @@ public class CxfServiceSpringBootApplication {
         endpoint.setProviders(providers);
         
         List<Object> serviceBeans = new ArrayList<Object>();
-        serviceBeans.add(new HelloServiceImpl1());
-        //serviceBeans.add(new ShoppingListApiImpl());
         serviceBeans.addAll(ctx.getBeansWithAnnotation(Path.class).values());
         
-        // Register 2 JAX-RS root resources supporting "/sayHello/{id}" and "/sayHello2/{id}" relative paths
         endpoint.setServiceBeans(serviceBeans);
         
         endpoint.setFeatures(Arrays.asList(new Swagger2Feature()));
